50ea436b434c26a2b7ab330310c3ea4c10d761fc
reeya3 <manandharreeya@gmail.com>
2023-09-08 10:43:34 +0545

    added user login feature

--- website/views.py
+++ website/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        username = request.POST['Username']
+        password = request.POST['Password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')
+        else:
+            messages.error(request, 'Incorrect username or password. please try again with correct credentials')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    return render(request, 'login.html' ,{})
+
+def logout_user(request):
+    return render(request, 'logout.html' ,{})
